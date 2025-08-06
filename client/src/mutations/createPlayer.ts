fca0f1ecf1ee6f4140e9a9c59223243c6c23d1fe
freemikka <36988403+freemikka@users.noreply.github.com>
2025-08-06 22:33:07 +0200

    toast message

--- client/src/mutations/createPlayer.ts
+++ client/src/mutations/createPlayer.ts
@@ -1,15 +1,19 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createPlayer } from "../services/playerService";
-
+import { toast } from "sonner";
 export const useCreatePlayer = () => {
     const queryClient = useQueryClient();
     return useMutation({
         mutationFn: createPlayer,
         onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ["teamsWithPlayers"] });
+            toast.success("Created new player!");
         },
         onError: (error) => {
             console.error("Failed creating player:", error.message);
+            toast.error(
+                `Error: ${error.message} Call your local webcie member..`
+            );
         },
     });
 };
